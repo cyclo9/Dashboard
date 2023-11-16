0a843eae181dc2a90ce65c4696fb40ebb7a69d0a
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2023-11-16 11:56:01 -0800

    Create config files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	// code snippet provided by https://github.com/gregrickaby/nextjs-github-pages
+	output: "export",
+	basePath: "/nextjs-github-pages",
+	images: {
+		unoptimized: true,
+	},
+}
 
 module.exports = nextConfig
