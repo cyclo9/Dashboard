d3c9e5445fab2d9e2321f00f87047b19c3ada573
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2023-11-17 00:19:11 -0800

    Update favicon.png to favicon.ico

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
 		<html lang="en">
-			<link rel="icon" href="/favicon.png" sizes="any" />
+			<link rel="icon" href="./favicon.ico" sizes="any" />
       <body>{children}</body>
     </html>
   )
