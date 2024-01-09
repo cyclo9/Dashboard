5e54339893a0d586d07c6bdac918f2b6e21f3736
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2024-01-08 18:25:12 -0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,9 +6,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-		<html lang="en">
-			<link rel="icon" href="./favicon.ico" sizes="any" />
-      <body>{children}</body>
+	  <html lang="en">
+		  <link rel="icon" href="./favicon.ico" sizes="any" />
+		  <body>{children}</body>
     </html>
   )
 }
